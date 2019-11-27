9ca40515b7ad7cd59132a2db58d562126552cec7
mae meier <maemeier@live.com>
2019-11-27 18:22:25 +0100

    install redux, created :reducers, actions

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SongList from "./songList";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <SongList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+const songsReducer = () => {
+  return [
+    { title: "love song", duration: "4.05" },
+    { title: "I'm yours", duration: "3.15" },
+    { title: "Lucky", duration: "4.13" },
+    { title: "When I see an elephant fly", duration: "3.72" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
